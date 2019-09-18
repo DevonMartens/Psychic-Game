836cbdaf953166c97d529066a7fb03f71def887b
Devon M Martens <52753632+DevonMartens@users.noreply.github.com>
2019-09-18 18:55:29 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-// var userGuess = document.onkeyup;
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessedLetters = [];
-// var currentGuess = [];
+
 
 
 
